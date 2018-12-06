dff10a4e77c8d4f66bdf2bc355abee9dc46a49b6
liuzhifeng2012 <zhifeng2012@163.com>
2018-12-06 18:32:19 +0800

    首页

---
+++ pages/template/navCustom/navCustom.js
@@ -0,0 +1,26 @@
+// pages/template/navCustom/navCustom.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    navShow: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
